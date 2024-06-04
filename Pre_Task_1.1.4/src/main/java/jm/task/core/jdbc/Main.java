61fc4d648a296d0cc2f8786f3939a60922b4b28f
isatsanarom <morozova18080108@gmail.com>
2024-06-04 14:52:04 +0300

    hiber1

--- Pre_Task_1.1.4/src/main/java/jm/task/core/jdbc/Main.java
+++ Pre_Task_1.1.4/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,8 @@
 package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import jm.task.core.jdbc.dao.*;
 
 import java.util.List;
 
